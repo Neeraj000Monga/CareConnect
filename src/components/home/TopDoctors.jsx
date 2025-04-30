7116e636c9e9793efcadc38c279853fc620e7cd9
Neeraj <Neeraj000Monga>
2025-04-30 19:09:05 +0530

    change doctor card

--- src/components/home/TopDoctors.jsx
+++ src/components/home/TopDoctors.jsx
@@ -1,348 +1,137 @@
-import React from "react";
-import doc1 from "../../assets/doc1.png";
-import doc2 from "../../assets/doc2.png";
-import doc3 from "../../assets/doc3.png";
-import doc4 from "../../assets/doc4.png";
-import doc5 from "../../assets/doc5.png";
-import doc6 from "../../assets/doc6.png";
-import doc7 from "../../assets/doc7.png";
-import doc8 from "../../assets/doc8.png";
-import doc9 from "../../assets/doc9.png";
-import doc10 from "../../assets/doc10.png";
-import doc11 from "../../assets/doc11.png";
-import doc12 from "../../assets/doc12.png";
-import doc13 from "../../assets/doc13.png";
-import doc14 from "../../assets/doc14.png";
-import doc15 from "../../assets/doc15.png";
+import React, { useState, useEffect } from "react";
 import {
   Box,
   Button,
-  Card,
-  CardContent,
   CardMedia,
+  Paper,
+  Stack,
   Typography,
   useTheme,
 } from "@mui/material";
 
-const doctors = [
-  {
-    _id: "doc1",
-    name: "Dr. Richard James",
-    image: doc1,
-    speciality: "General physician",
-    degree: "MBBS",
-    experience: "4 Years",
-    about:
-      "Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies. Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies.",
-    fees: 50,
-    address: {
-      line1: "17th Cross, Richmond",
-      line2: "Circle, Ring Road, London",
-    },
-  },
-  {
-    _id: "doc2",
-    name: "Dr. Emily Larson",
-    image: doc2,
-    speciality: "Gynecologist",
-    degree: "MBBS",
-    experience: "3 Years",
-    about:
-      "Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies. Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies.",
-    fees: 60,
-    address: {
-      line1: "27th Cross, Richmond",
-      line2: "Circle, Ring Road, London",
-    },
-  },
-  {
-    _id: "doc3",
-    name: "Dr. Sarah Patel",
-    image: doc3,
-    speciality: "Dermatologist",
-    degree: "MBBS",
-    experience: "1 Years",
-    about:
-      "Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies. Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies.",
-    fees: 30,
-    address: {
-      line1: "37th Cross, Richmond",
-      line2: "Circle, Ring Road, London",
-    },
-  },
-  {
-    _id: "doc4",
-    name: "Dr. Christopher Lee",
-    image: doc4,
-    speciality: "Pediatricians",
-    degree: "MBBS",
-    experience: "2 Years",
-    about:
-      "Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies. Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies.",
-    fees: 40,
-    address: {
-      line1: "47th Cross, Richmond",
-      line2: "Circle, Ring Road, London",
-    },
-  },
-  {
-    _id: "doc5",
-    name: "Dr. Jennifer Garcia",
-    image: doc5,
-    speciality: "Neurologist",
-    degree: "MBBS",
-    experience: "4 Years",
-    about:
-      "Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies. Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies.",
-    fees: 50,
-    address: {
-      line1: "57th Cross, Richmond",
-      line2: "Circle, Ring Road, London",
-    },
-  },
-  {
-    _id: "doc6",
-    name: "Dr. Andrew Williams",
-    image: doc6,
-    speciality: "Neurologist",
-    degree: "MBBS",
-    experience: "4 Years",
-    about:
-      "Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies. Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies.",
-    fees: 50,
-    address: {
-      line1: "57th Cross, Richmond",
-      line2: "Circle, Ring Road, London",
-    },
-  },
-  {
-    _id: "doc7",
-    name: "Dr. Christopher Davis",
-    image: doc7,
-    speciality: "General physician",
-    degree: "MBBS",
-    experience: "4 Years",
-    about:
-      "Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies. Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies.",
-    fees: 50,
-    address: {
-      line1: "17th Cross, Richmond",
-      line2: "Circle, Ring Road, London",
-    },
-  },
-  {
-    _id: "doc8",
-    name: "Dr. Timothy White",
-    image: doc8,
-    speciality: "Gynecologist",
-    degree: "MBBS",
-    experience: "3 Years",
-    about:
-      "Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies. Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies.",
-    fees: 60,
-    address: {
-      line1: "27th Cross, Richmond",
-      line2: "Circle, Ring Road, London",
-    },
-  },
-  {
-    _id: "doc9",
-    name: "Dr. Ava Mitchell",
-    image: doc9,
-    speciality: "Dermatologist",
-    degree: "MBBS",
-    experience: "1 Years",
-    about:
-      "Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies. Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies.",
-    fees: 30,
-    address: {
-      line1: "37th Cross, Richmond",
-      line2: "Circle, Ring Road, London",
-    },
-  },
-  {
-    _id: "doc10",
-    name: "Dr. Jeffrey King",
-    image: doc10,
-    speciality: "Pediatricians",
-    degree: "MBBS",
-    experience: "2 Years",
-    about:
-      "Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies. Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies.",
-    fees: 40,
-    address: {
-      line1: "47th Cross, Richmond",
-      line2: "Circle, Ring Road, London",
-    },
-  },
-  {
-    _id: "doc11",
-    name: "Dr. Zoe Kelly",
-    image: doc11,
-    speciality: "Neurologist",
-    degree: "MBBS",
-    experience: "4 Years",
-    about:
-      "Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies. Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies.",
-    fees: 50,
-    address: {
-      line1: "57th Cross, Richmond",
-      line2: "Circle, Ring Road, London",
-    },
-  },
-  {
-    _id: "doc12",
-    name: "Dr. Patrick Harris",
-    image: doc12,
-    speciality: "Neurologist",
-    degree: "MBBS",
-    experience: "4 Years",
-    about:
-      "Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies. Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies.",
-    fees: 50,
-    address: {
-      line1: "57th Cross, Richmond",
-      line2: "Circle, Ring Road, London",
-    },
-  },
-  {
-    _id: "doc13",
-    name: "Dr. Chloe Evans",
-    image: doc13,
-    speciality: "General physician",
-    degree: "MBBS",
-    experience: "4 Years",
-    about:
-      "Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies. Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies.",
-    fees: 50,
-    address: {
-      line1: "17th Cross, Richmond",
-      line2: "Circle, Ring Road, London",
-    },
-  },
-  {
-    _id: "doc14",
-    name: "Dr. Ryan Martinez",
-    image: doc14,
-    speciality: "Gynecologist",
-    degree: "MBBS",
-    experience: "3 Years",
-    about:
-      "Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies. Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies.",
-    fees: 60,
-    address: {
-      line1: "27th Cross, Richmond",
-      line2: "Circle, Ring Road, London",
-    },
-  },
-  {
-    _id: "doc15",
-    name: "Dr. Amelia Hill",
-    image: doc15,
-    speciality: "Dermatologist",
-    degree: "MBBS",
-    experience: "1 Years",
-    about:
-      "Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies. Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies.",
-    fees: 30,
-    address: {
-      line1: "37th Cross, Richmond",
-      line2: "Circle, Ring Road, London",
-    },
-  },
-];
-
 const TopDoctors = () => {
-
   const theme = useTheme();
+  const [doctors, setDoctors] = useState([]);
+  const [doctorCount, setDoctorCount] = useState(6);
+
+  console.log("doctors", doctors)
+
+  useEffect(() => {
+    const fetchDoctors = async () => {
+      try {
+        const response = await fetch(
+          "https://67d826719d5e3a10152d9ddf.mockapi.io/CareConnect/Doctor"
+        );
+        const data = await response.json();
+        setDoctors(data);
+      } catch (error) {
+        console.error("Error fetching doctors:", error);
+      }
+    };
+
+    fetchDoctors();
+  }, []);
+
+  const handleShowMore = () => {
+    setDoctorCount((prev) => prev + 6);
+  };
 
   return (
-    <Box
-      gap={4}
-      mx={{ md: 5 }}
-      display="flex"
-      flexDirection="column"
-      alignItems="center"
-      color="text.primary"
-    >
-      <Typography sx={{ fontSize: { xs: "37px", md: "3rem" } }} fontWeight="medium" >
+    <Stack mx={{ md: 5, gap: 4, alignItems: "center" }}>
+      <Typography sx={{ fontSize: { xs: "37px", md: "3rem" } }} fontWeight="medium">
         Top Doctors to Book
       </Typography>
-      <Typography
-        textAlign="center"
-        sx={{ width: { sm: "33%" } }}
-        color="text.secondary"
-      >
+      <Typography textAlign="center" sx={{ width: { sm: "33%" } }} color="text.secondary">
         Simply browse through our extensive list of trusted doctors.
       </Typography>
 
-      <Box sx={{ width: "100%", display: "flex", flexWrap: "wrap", gap: "20px" }}>
-        {doctors.slice(0, 10).map((item, index) => (
-          <Card
-            sx={{
-              width: { xs: "100%", sm: "215px" },
-              border: theme.palette.mode === "dark" ? "1px solid #3c3b3b" : "1px solid #c9d8ff",
-              borderRadius: 2,
-              overflow: "hidden",
-              cursor: "pointer",
-              transition: "transform 0.5s",
-              "&:hover": { transform: "translateY(-10px)" },
-            }}
-            key={index}
-          >
-            <CardMedia
-              component="img"
-              image={item.image}
-              alt=""
-              sx={{ bgcolor: "#eaefff", height: { xs: "300px", sm: "218px" }, width: "100%" }}
-            />
-            <CardContent>
-              <Box
-                display="flex"
-                alignItems="center"
-                gap={1}
-                textAlign="center"
-                color="#22c55e"
-              >
-                <Box
-                  width={8}
-                  height={8}
-                  bgcolor="#22c55e"
-                  borderRadius="50%"
-                />
-                <Typography variant="body2">Available</Typography>
-              </Box>
-              <Typography
-                variant="h6"
-                fontWeight="medium"
-                color="text.primary"
-              >
-                {item.name}
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                {item.speciality}
-              </Typography>
-            </CardContent>
-          </Card>
-        ))}
-      </Box>
-
-      <Button
-        variant="contained"
-        sx={{
-          px: 6,
-          mt: 2,
-          py: 1.5,
-          fontWeight: "600",
-          borderRadius: "50px",
-          textTransform: "none",
-          color: "text.secondary",
-          backgroundColor: theme.palette.mode === "dark" ? "#212121" : "#e8eced",
+      <Stack
+        style={{
+          gap: "30px",
+          flexWrap: "wrap",
+          paddingTop: "30px",
+          flexDirection: "row",
+          justifyContent: "space-evenly",
         }}
       >
-        more
-      </Button>
-    </Box>
+        {doctors.slice(0, doctorCount).map((item, index) => (
+          <Box key={index} style={{ width: "260px", height: "310px" }}>
+            <Stack>
+              <Box style={{ position: "absolute", background: "#eaefff", borderRadius: "4px" }}>
+                {item?.profilePic && (
+                  <CardMedia
+                    component="img"
+                    image={item?.profilePic}
+                    sx={{
+                      width: "258px",
+                      height: "240px",
+                      borderRadius: "4px"
+                    }}
+                    alt={item?.name}
+                  />
+                )}
+              </Box>
+              <Box
+                style={{
+                  width: "260px",
+                  display: "flex",
+                  justifyContent: "center",
+                  position: "relative",
+                  top: "170px",
+                }}
+              >
+                <Paper
+                  style={{
+                    justifyContent: "space-between",
+                    border: "1px solid #d5d4d4",
+                    flexDirection: "column",
+                    height: "130px",
+                    display: "flex",
+                    padding: "10px",
+                    width: "220px",
+                  }}
+                >
+                  <Box>
+                    <Box display="flex" alignItems="center" gap={1} textAlign="center" color="#22c55e">
+                      <Box width={8} height={8} bgcolor="#22c55e" borderRadius="50%" />
+                      <Typography variant="body2">Available</Typography>
+                    </Box>
+                    <Typography variant="h6" fontWeight="medium" color="text.primary">
+                      {item?.name}
+                    </Typography>
+                    <Typography sx={{ fontSize: "14px", textTransform: "capitalize" }} color="text.secondary">
+                      {item?.degree}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      {item?.specialization}
+                    </Typography>
+                  </Box>
+                </Paper>
+              </Box>
+            </Stack>
+          </Box>
+        ))}
+      </Stack>
+
+      {doctorCount < doctors.length && (
+        <Button
+          variant="contained"
+          onClick={handleShowMore}
+          sx={{
+            mt: 2,
+            px: 6,
+            py: 1.5,
+            fontWeight: "600",
+            borderRadius: "50px",
+            textTransform: "none",
+            color: "text.secondary",
+            backgroundColor: theme.palette.mode === "dark" ? "#212121" : "#e8eced",
+          }}
+        >
+          more
+        </Button>
+      )}
+    </Stack>
   );
 };
 
